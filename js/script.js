dd876699744ddfd8f5c5fa92c18c5aa5e7d6b15d
DamianoLandi <damianolandi01@gmail.com>
2021-07-06 17:15:24 +0200

    Settaggio Variabili e Richiesta Input

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Impostazione Array
+
+var list = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+
+
+//Richiesta Input
+
+do{
+    var input = prompt("Inserisci il tuo cognome");
+}while(!isNaN(input));
+
